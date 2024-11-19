ed4ec80befcb0a521025105491e7737a8ab8911d
mmh <you@example.com>
2024-11-19 09:23:52 +0700

    Not using Arraylist

--- SieuThiMini/Receipt.java
+++ SieuThiMini/Receipt.java
@@ -1,32 +1,28 @@
-import java.util.List;
 
 
 public class Receipt {
     private int receiptId;
     private Transaction transaction;
-    private List<Item> items;
 
-
-    public Receipt(int receiptId, Transaction transaction){
+    public Receipt(int receiptId, Transaction transaction) {
         this.receiptId = receiptId;
         this.transaction = transaction;
-        this.items = transaction.getItems();
-
     }
+
     public void print() {
         System.out.println("Receipt ID: " + receiptId);
         System.out.println("Transaction ID: " + transaction.getId());
         System.out.println("Date: " + transaction.getDate());
         System.out.println("Items:");
-        for (Item item : items) {
-            System.out.println(" - " + item.getName() + ": đ" + item.getPrice());
+        Item[] items = transaction.getItems();
+        for (int i = 0; i < items.length; i++) {
+            System.out.println(" - " + items[i].getName() + ": đ" + items[i].getPrice());
         }
         System.out.println("Total Amount: đ" + transaction.getTotal());
     }
 
-
     public int getReceiptId() {
-        return this.receiptId;
+        return receiptId;
     }
 
     public void setReceiptId(int receiptId) {
@@ -34,15 +30,10 @@ public class Receipt {
     }
 
     public Transaction getTransaction() {
-        return this.transaction;
+        return transaction;
     }
 
     public void setTransaction(Transaction transaction) {
         this.transaction = transaction;
-        this.items= transaction.getItems();
     }
-
- 
-
-
 }
--- SieuThiMini/Transaction.java
+++ SieuThiMini/Transaction.java
@@ -1,37 +1,49 @@
 import java.util.Date;
-import java.util.List;
-import java.util.ArrayList;
 
 public class Transaction {
     private int id;
     private Date date;
     private double totalAmount;
-    private List<Item> items;
+    private Item[] items;
+    private int itemCount;
 
-    public Transaction(int id, Date date){
+    public Transaction(int id, Date date) {
         this.id = id;
-        this.date=date;
-        this.totalAmount= 0.0;
-        this.items = new ArrayList<>();
+        this.date = date;
+        this.totalAmount = 0.0;
+        this.items = new Item[100];
+        this.itemCount = 0;
     }
 
-    public void addItem(Item item){
-        items.add(item);
-        totalAmount += item.getPrice();
-    }
-
-    public void removeItem(Item item){
-        if(items.remove(item)){
-            totalAmount -= item.getPrice();
+    public void addItem(Item item) {
+        if (itemCount < items.length) {
+            items[itemCount++] = item;
+            totalAmount += item.getPrice();
+        } else {
+            System.out.println("Can't add more items.");
         }
     }
 
-    public double getTotal(){
+    public void removeItem(Item item) {
+        for (int i = 0; i < itemCount; i++) {
+            if (items[i].equals(item)) {
+                totalAmount -= items[i].getPrice();
+                // Shift items to fill the gap
+                for (int j = i; j < itemCount - 1; j++) {
+                    items[j] = items[j + 1];
+                }
+                items[--itemCount] = null;
+                break;
+            }
+        }
+    }
+
+    public double getTotal() {
         return totalAmount;
     }
 
     public int getId() {
-        return this.id;
+        return id;
     }
 
     public void setId(int id) {
@@ -39,20 +51,25 @@ public class Transaction {
     }
 
     public Date getDate() {
-        return this.date;
+        return date;
     }
 
     public void setDate(Date date) {
         this.date = date;
     }
 
-    public List<Item> getItems() {
-        return this.items;
+    public Item[] getItems() {
+        Item[] currentItems = new Item[itemCount];
+        System.arraycopy(items, 0, currentItems, 0, itemCount);
+        return currentItems;
     }
 
-    public void setItems(List<Item> items) {
+    public void setItems(Item[] items) {
         this.items = items;
+        this.itemCount = items.length;
+        this.totalAmount = 0;
+        for (Item item : items) {
+            this.totalAmount += item.getPrice();
+        }
     }
-
-    
 }
